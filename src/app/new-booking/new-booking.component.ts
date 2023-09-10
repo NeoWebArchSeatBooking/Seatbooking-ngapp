21168e40721469885568461bba0b24717802968c
Sreeram Basam <sreeram.basam@gmail.com>
2023-09-10 15:57:58 +0530

    Added options for new seat booking

--- src/app/new-booking/new-booking.component.ts
+++ src/app/new-booking/new-booking.component.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
+import { CompanyInfoService } from '../services/company-info.service';
+
 @Component({
   selector: 'app-new-booking',
   templateUrl: './new-booking.component.html',
   styleUrls: ['./new-booking.component.scss']
 })
 export class NewBookingComponent {
+  selectedSeat: any = {};
+  availableSeats: any;
+  locations: any;
+  blocks: any;
+  floors: any;
+  seats: any;
+
+  constructor(
+    private infraService: CompanyInfoService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  private getData() {
+    this.infraService.fetchSeatingInformation().subscribe((res) => {
+      this.availableSeats = res.seats;
+      this.locations = res.infras;
+      console.log(this.locations);
+    });
+  }
+
+  locationChange(x: any): void {
+    this.blocks = this.locations.find((loc: any) => loc.locationId === x.value).blocks;
+    this.floors = [];
+    this.seats = [];
+  }
+
+  blockChange(y: any) {
+    this.floors = this.blocks.find((bloc: any) => bloc.blockId === y.value).floors;
+    this.seats = [];
+  }
+
+  floorChange(y: any) {
+    this.infraService.fetchAvailableSeats(this.selectedSeat).subscribe((res: any) => {
+      this.seats = res;
+    });
+    // this.seats = this.availableSeats.filter((seat: any) => {
+    //   return seat.locationId === this.selectedSeat.location &&
+    //     seat.blockId === this.selectedSeat.block &&
+    //     seat.floorId === y.value;
+    // });
+  }
+
+  seatChange(y: any): void {
+
+  }
 
 }
--- src/app/services/company-info.service.ts
+++ src/app/services/company-info.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../shared/service/api.service';
 import { IAPIConfiguration } from '../shared/service/interfaces/i-configuration';
 import { map, of } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 // import { IAPIConfiguration } from '../shared/service/interfaces/i-configuration';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { map, of } from 'rxjs';
 })
 export class CompanyInfoService {
 
-  seatInformation:any;
+  seatInformation: any;
   constructor(private apiService: ApiService) { }
 
   fetchSeatingInformation() {
@@ -20,12 +21,12 @@ export class CompanyInfoService {
     return this.apiService.httpGet('1140606227507437568', null, config);
     */
 
-    const config:IAPIConfiguration = {
+    const config: IAPIConfiguration = {
       group: 'infra',
       key: 'facilities',
       overrideResourcePath: 'idp'
     };
-    if(this.seatInformation) {
+    if (this.seatInformation) {
       return of(this.seatInformation);
     }
     return this.apiService.httpGet('facilities', null, config).pipe((map(res => {
@@ -34,8 +35,22 @@ export class CompanyInfoService {
     })));
   }
 
+  fetchAvailableSeats(seatFilters: { locationId: any, blockId: any, floorId: any }) {
+    const config: IAPIConfiguration = {
+      group: 'seats',
+      key: 'seats',
+      overrideResourcePath: 'idp'
+    };
+    const params = new HttpParams();
+    params.append('locationId', seatFilters.locationId);
+    params.append('blockId', seatFilters.blockId);
+    params.append('floorId', seatFilters.floorId);
+    return this.apiService.httpGet('seats', params, config);
+  }
+
+
   getInfraOptions() {
-    return  of({
+    return of({
       "items": [
         {
           "key": "infras",
