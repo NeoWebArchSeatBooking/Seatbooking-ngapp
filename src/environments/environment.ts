adb12f32b4f7b82e08eac0ac3d384cca9c1fe87f
DhamoSG <sgd.daran@gmail.com>
2023-09-10 06:35:52 +0000

    dhamo: proxy config change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     baseURL: '',
-    resourcePath: 'sbapi/v1',
+    resourcePath: 'api/v1',
     isMock: false,
     clientKey: '601004974015-n3jgfmkijvivhsdc5ajusln88k819bmj'
 };
