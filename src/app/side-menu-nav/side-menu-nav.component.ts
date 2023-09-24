e8b134f1ab21ba648d9b6f4fabd5428a2ebc897b
DhamoSG <sgd.daran@gmail.com>
2023-09-24 15:04:31 +0000

    ss

--- src/app/side-menu-nav/side-menu-nav.component.ts
+++ src/app/side-menu-nav/side-menu-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,APP_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { IdleService } from '../services/idle.service';
