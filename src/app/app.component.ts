036c62ee2b4880c1db00d9e51c8e8f15d14d5da7
nidhincg <nidhincg24@gmail.com>
2023-09-29 20:52:21 +0530

    fix conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { distinctUntilChanged, filter } from 'rxjs';
 import { EventService } from '././event.service';
+<<<<<<< Updated upstream
 import { Subscription } from 'rxjs';
+=======
+import { AuthService } from './services/auth.service';
+import { IdleService } from './services/idle.service';
+import { UtilityService } from './shared/service/utility/utility.service';
+>>>>>>> Stashed changes
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +18,7 @@ export class AppComponent {
   title = 'seatBooking';
   receivedMessage: boolean = false;
   loggedIn = false;
+<<<<<<< Updated upstream
   user = 'NA';
   role ='';
   subscriptions: Subscription[] = [];
@@ -22,6 +31,37 @@ export class AppComponent {
       this.user = eventData.user;
       this.role = eventData.role;
     }));
+=======
+  isAdmin = false;
+  displayLogin;
+  constructor(
+    private authService: AuthService,
+    private eventService: EventService,
+    private idleService: IdleService,
+    private utilityService: UtilityService,
+    private router: Router
+  ) {
+    this.idleCheck();
+   
+   }
+
+  ngOnInit(): void {
+    this.loggedIn = this.authService.isLoggedIn();
+    this.user = this.authService.getUSerDetails();
+      
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      distinctUntilChanged()
+    ).subscribe((route:any) => {
+      this.displayLogin = route.url !== "/login";      
+    });
+    
+    this.eventService.message$.subscribe((isLoggedIn:boolean) => {
+      this.isAdmin = this.authService.isAdmin()
+      this.loggedIn =isLoggedIn;
+      this.user = this.authService.getUSerDetails();
+    });
+>>>>>>> Stashed changes
   }
 
   ngOnDestroy(): void {
--- src/app/side-menu-nav/side-menu-nav.component.ts
+++ src/app/side-menu-nav/side-menu-nav.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+<<<<<<< Updated upstream
 import { EventService } from './../event.service';
 import { JwtService } from '../jwt.service';
+=======
+import { AuthService } from '../services/auth.service';
+import { IdleService } from '../services/idle.service';
+import { EventService } from '../event.service';
+>>>>>>> Stashed changes
 @Component({
   selector: 'app-side-menu-nav',
   templateUrl: './side-menu-nav.component.html',
   styleUrls: ['./side-menu-nav.component.scss']
 })
+<<<<<<< Updated upstream
 export class SideMenuNavComponent {
   constructor( private eventService: EventService, private router: Router,   private jwtService: JwtService,) {}
   showAllBooking: boolean = false;
   role ='';
+=======
+export class SideMenuNavComponent implements OnInit{
+  isAdmin:boolean;
+
+  constructor( 
+    private router: Router,
+    private idleService: IdleService,
+    private authService: AuthService,
+    private eventServie: EventService ) {}
+
+>>>>>>> Stashed changes
   ngOnInit(): void {
       this.role = localStorage.getItem('Role');
       if (this.role == 'admin'){
@@ -27,8 +45,15 @@ export class SideMenuNavComponent {
     this.jwtService.removeToken();
     // @ts-ignore
     google.accounts.id.disableAutoSelect();
+<<<<<<< Updated upstream
     const eventData = { loggedIn: false, user: 'NA', role: 'NA'};
     this.eventService.emitEvent(eventData);
+=======
+
+    this.authService.logOut();
+    this.idleService.clear();
+    this.eventServie.showHideMenu(false);
+>>>>>>> Stashed changes
     this.router.navigate(['login']);
   }
 }
